e1151985a9187cea78a84df68b259b422c22457e
WesleyVianna <darkwesley1531@gmail.com>
2026-01-05 10:49:08 -0300

    chore: inicializa projeto ASP.NET MVC e README

---
+++ PontoTuristicoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PontoTuristicoApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
