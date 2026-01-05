cd6ab12742970f98375737dd2b867fd564665da8
WesleyVianna <darkwesley1531@gmail.com>
2026-01-05 15:24:50 -0300

    fix: corrige erro no controller de pontos turísticos

--- PontoTuristicoApp/Controllers/PontosTuristicosController.cs
+++ PontoTuristicoApp/Controllers/PontosTuristicosController.cs
@@ -5,20 +5,41 @@ using System.Linq;
 
 namespace PontoTuristicoApp.Controllers
 {
-    public class PontosTuristicosContoller : Controller
+    public class PontosTuristicosController : Controller
     {
         private readonly AppDbContext _context;
 
-        public PontosTuristicosContoller(AppDbContext context)
+        public PontosTuristicosController(AppDbContext context)
         {
             _context = context;
         }
 
         public IActionResult Index()
         {
-            var pontos = _context.PontosTuristicos.OrderByDescending(p => p.DataInclusao).ToImmutableList();
+            var pontos = _context.PontosTuristicos.OrderByDescending(p => p.DataInclusao).ToList();
 
             return View(pontos);
         }
+    
+        // GET: PontosTuristicos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PontosTuristicos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Models.PontoTuristico ponto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.PontosTuristicos.Add(ponto);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(ponto);
+        } 
     }
 }
